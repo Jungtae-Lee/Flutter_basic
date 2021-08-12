9d901c86057758d55d2a9fc8fedbef4f35c5d393
FlutLab.io <>
2021-08-12 17:17:24 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, primaryColor: Colors.white, accentColor: Colors.black),
       home: RootPage(),
     );
   }
