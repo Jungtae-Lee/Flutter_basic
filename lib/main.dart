8515c4db8e87c2ad7cb755c73e1d0cae0f3c2d06
FlutLab.io <>
2021-08-12 13:31:28 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
-import 'root_page.dart';
 
-void main() {
-  runApp(MyApp());
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: RootPage(),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(onTap: _onItemTapped, currentIndex: _selectedIndex, items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Account')),
+      ]),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
