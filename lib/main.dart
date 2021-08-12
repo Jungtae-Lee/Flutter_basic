1b67676b70b5ffeb92c5a1ee4a2d1263d0c66e91
FlutLab.io <>
2021-08-12 13:15:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'cupertino_page.dart';
+import 'root_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,63 +10,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: HelloPage('!!Hello World!!'));
-  }
-}
-
-class HelloPage extends StatefulWidget {
-  final String title;
-
-  HelloPage(this.title) : super();
-
-  @override
-  _HelloPageState createState() => _HelloPageState();
-}
-
-class _HelloPageState extends State<HelloPage> {
-  String _message = 'Hello World';
-  int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(child: Icon(Icons.add), onPressed: _changeMessage),
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_message, style: TextStyle(fontSize: 30)),
-            Text('$_counter', style: TextStyle(fontSize: 30)),
-            RaisedButton(
-              child: Text('화면 이동'),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => CupertinoPage()));
-              },
-            )
-          ],
-        )));
-  }
-
-  void _changeMessage() {
-    setState(() {
-      _message = '헬로 월드';
-      _counter++;
-    });
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: RootPage(),
+    );
   }
 }
