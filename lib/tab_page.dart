1bfa07f5f5a74932333eb9ddbc63123286a6a10b
FlutLab.io <>
2021-08-12 16:32:19 +0000

    Autosave

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -8,9 +8,16 @@ class TabPage extends StatefulWidget {
 class _TabPageState extends State<TabPage> {
   int _selectedIndex = 0;
 
+  List _pages = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(onTap: _onItemTapped, currentIndex: _selectedIndex, items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
         BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
